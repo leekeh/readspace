c63f9342f0889bed60512d10857cb0868661f534
leekeh <71131586+leekeh@users.noreply.github.com>
2024-03-24 21:43:00 +0100

    feat: document browser support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit()],
+  build: {
+    target: "es2018",
+  },
+
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+  },
 });
