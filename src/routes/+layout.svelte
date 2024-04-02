382b3fae457a5c131a91da04a7d0d5eaa721b4c9
leekeh <71131586+leekeh@users.noreply.github.com>
2024-04-02 18:44:26 +0200

    move reset styles to static file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
-  import { initializeEnvironmentStores } from "@stores";
+  import { initializeEnvironmentStores, settings } from "@stores";
   import { onMount } from "svelte";
   export let data;
   $: ({ title } = data);
-  const color = "#19483a";
   onMount(() => {
     initializeEnvironmentStores();
   });
 </script>
 
 <svelte:head>
-  <meta name="theme-color" content={color} />
+  <meta name="theme-color" content={$settings.accent} />
   <title>ReadSpace - {title}</title>
 </svelte:head>
 <slot />
@@ -38,141 +37,5 @@
     --border-radius: 2rem;
     --border: var(--border-width) solid var(--foreground);
     accent-color: var(--highlight-color);
-  }
-
-  /* CSS Reset
-	http://meyerweb.com/eric/tools/css/reset/ 
-   v2.0 | 20110126
-   License: none (public domain)
-*/
-
-  :global(
-      html,
-      body,
-      div,
-      span,
-      applet,
-      object,
-      iframe,
-      h1,
-      h2,
-      h3,
-      h4,
-      h5,
-      h6,
-      p,
-      blockquote,
-      pre,
-      a,
-      abbr,
-      acronym,
-      address,
-      big,
-      cite,
-      code,
-      del,
-      dfn,
-      em,
-      img,
-      ins,
-      kbd,
-      q,
-      s,
-      samp,
-      small,
-      strike,
-      strong,
-      sub,
-      sup,
-      tt,
-      var,
-      b,
-      u,
-      i,
-      center,
-      dl,
-      dt,
-      dd,
-      ol,
-      ul,
-      li,
-      fieldset,
-      form,
-      label,
-      legend,
-      table,
-      caption,
-      tbody,
-      tfoot,
-      thead,
-      tr,
-      th,
-      td,
-      article,
-      aside,
-      canvas,
-      details,
-      embed,
-      figure,
-      figcaption,
-      footer,
-      header,
-      hgroup,
-      menu,
-      nav,
-      output,
-      ruby,
-      section,
-      summary,
-      time,
-      mark,
-      audio,
-      video
-    ) {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font: inherit;
-    vertical-align: baseline;
-    box-sizing: border-box;
-  }
-  /* HTML5 display-role reset for older browsers */
-  :global(
-      article,
-      aside,
-      details,
-      figcaption,
-      figure,
-      footer,
-      header,
-      hgroup,
-      menu,
-      nav,
-      section
-    ) {
-    display: block;
-  }
-  :global(body) {
-    line-height: 1;
-  }
-  :global(ol, ul) {
-    list-style: none;
-  }
-  :global(blockquote, q) {
-    quotes: none;
-  }
-  :global(blockquote:before, blockquote:after, q:before, q:after) {
-    content: "";
-    content: none;
-  }
-  :global(table) {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-
-  :global(a) {
-    color: inherit;
-    text-decoration: none;
   }
 </style>
